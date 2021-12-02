3abedf277262b1ea4ec6c0b7d64ff65e043da3c7
casperkristiansen <71253446+casperkristiansen@users.noreply.github.com>
2021-12-02 10:57:47 +0100

    EventDetails

--- app/src/main/java/com/example/furadminapp/ui/events/EventDetails.java
+++ app/src/main/java/com/example/furadminapp/ui/events/EventDetails.java
@@ -1,4 +1,16 @@
 package com.example.furadminapp.ui.events;
 
-public class EventDetails {
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.furadminapp.R;
+
+public class EventDetails extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_event_details);
+    }
 }
